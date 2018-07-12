74303b1a819a9d4f88a30d4db161de965800fa38
maxim <max.gavrushenko.tirare@gmail.com>
2018-07-12 16:09:28 +0300

    Add delete method

--- app/Http/Controllers/Admin/CategoryController.php
+++ app/Http/Controllers/Admin/CategoryController.php
@@ -100,5 +100,7 @@ class CategoryController extends Controller
     public function destroy(Category $category)
     {
         //
+        $category->delete();
+        return redirect()->route('admin.category.index');
     }
 }
