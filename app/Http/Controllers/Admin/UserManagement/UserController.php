eb870d239a64554c52940d4d892a719e39e5abee
maxim <max.gavrushenko.tirare@gmail.com>
2018-09-11 16:21:12 +0300

    Add delete user

--- app/Http/Controllers/Admin/UserManagement/UserController.php
+++ app/Http/Controllers/Admin/UserManagement/UserController.php
@@ -125,5 +125,7 @@ class UserController extends Controller
     public function destroy(User $user)
     {
         //
+        $user->delete();
+        return redirect()->route('admin.user_management.user.index');
     }
 }
